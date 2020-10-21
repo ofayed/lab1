d8c584cff290dc0cf4fcfd9e3b041fa5f487670a
ofayed <70962709+ofayed@users.noreply.github.com>
2020-10-21 22:41:20 +0200

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void display(int a[20][20],int n);
+void initialize(int a[20][20],int n);
+void closeplace(int r, int c, int a[20][20],int n);
+
+#endif
